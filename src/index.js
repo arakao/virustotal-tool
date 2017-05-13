dd4a7e4a5922dbac22768f2f6a7b212831065d37
kikou5656 <kikou.raku@gmail.com>
2017-05-14 02:49:51 +0900

    エラーデータもファイルに出力するよう修正

--- src/index.js
+++ src/index.js
@@ -55,8 +55,31 @@ function getResultHeader() {
 	return result_header;
 }
 
+// エラーデータのレコードを取得
+function getErrRecord(data) {
+	const av_list = config.appconf.av_list;
+	const result = [];
+	// [0]sha256
+	result.push(data.resource);
+	// [1]sha1
+	result.push('');
+	// [2]md5
+	result.push('');
+	for (let av_idx = 0; av_idx < av_list.length; av_idx++) {
+		result.push('');
+		result.push('');
+	}
+	return result;
+}
+
 function getResultRecord(data) {
 	const av_list = config.appconf.av_list;
+	data = Object.assign({
+		'sha256': '',
+		'sha1': '',
+		'md5': '',
+		'scans': {},
+	}, data);
 
 	const result = [];
 	// [0]sha256
@@ -67,9 +90,15 @@ function getResultRecord(data) {
 	result.push(data.md5);
 	// av_list loop
 	for(let av_idx = 0, av_len = av_list.length; av_idx < av_len; av_idx++) {
-		const av_data = Object.assign({'detected': '', result: ''}, data.scans[av_list[av_idx]]);
-		result.push(av_data.detected);
-		result.push(av_data.result);
+		if (av_list[av_idx] in data.scans) {
+			const av_data = Object.assign({'detected': '', 'result': ''}, data.scans[av_list[av_idx]]);
+			result.push(av_data.detected);
+			result.push(av_data.result);
+		}
+		else {
+			result.push('');
+			result.push('');
+		}
 	}
 	return result;
 }
@@ -113,7 +142,8 @@ co(function* () {
 		if (data.response_code === 1) {
 			result_list.push(getResultRecord(data));
 		} else {
-			logger.app.warn(`request failed: ${data.verbose_msg}`)
+			logger.app.warn(`request failed: ${data.verbose_msg}`);
+			result_list.push(getErrRecord(data));
 		}
 	}
 
